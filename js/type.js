d530f2b8886a8f3fcc3e0f961c975f1b91f0c5ce
mohdkamleen <kamleenmohd@gmail.com>
2021-07-03 07:47:37 +0530

    html

---
+++ js/type.js
@@ -0,0 +1,10 @@
+
+    var typed = new Typed('.type_text', {
+            strings: ["", " web developer", "  web designer", "Programmer", "Studet", "IOT Project", "Arduino Project" ],
+            loop: true,
+            backSpeed: 30,
+            typeDelay: 1000,
+            cursorChar: '_',
+            typeSpeed: 100
+
+            }); 
